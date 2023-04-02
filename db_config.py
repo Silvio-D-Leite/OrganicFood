97efae05de449e68b449f1aea5089d370baef241
Silvio_Leite <silviosdm18@hotmail.com>
2023-04-02 16:24:44 -0300

    Primeiro GIT

--- db_config.py
+++ db_config.py
@@ -6,7 +6,7 @@ try:
     conn = mysql.connector.connect(
         host='127.0.0.1',
         user='root',
-        password='753129senha'
+        password='xxxxxx'
     )
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
